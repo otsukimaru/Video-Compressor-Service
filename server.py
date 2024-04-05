73bc5599c7d8bab75de3b63d5c1bc896c7e19eb5
otsukimaru <otsukinandesu@gmail.com>
2024-04-06 00:18:15 +0900

    一旦ffmpgを使った関数を作成

--- server.py
+++ server.py
@@ -1,9 +1,37 @@
 import socket 
 import os
+import sys
+import subprocess
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = '0.0.0.0'
 server_port = 9001
+
+# 選択したビデオを圧縮する
+def compress_video(videos):
+    for video in videos:
+        subprocess.call('ffmpeg -i '+video+' -crf 18 '+video[:-4]+'_comp.mp4', shell=True)
+
+# 選択したビデオの解像度を変更する
+def change_video_resolution(videos):
+    for video in videos:
+        subprocess.call('ffmpeg -i '+video+' -vf scale=1280:720 '+video[:-4]+'_resized.mp4', shell=True)
+
+# 選択したビデオのアスペクト比を変更する
+def change_video_aspect_ratio(videos):
+    for video in videos:
+        subprocess.call('ffmpeg -i '+video+' -c "-aspect 16:9 '+video[:-4]+'_aspect.mp4', shell=True)
+
+# 選択したビデオをMP3に変換する
+def translate_to_mp3(videos):
+    for video in videos:
+        subprocess.call('ffmpeg -i '+video+' -q:a 0 -map a '+video[:-4]+'.mp3', shell=True)
+
+# 選択した画像をGIFに変換する        
+def pick_up_video_to_gif(videos, start, finish):
+    for video in videos:
+        subprocess.call('ffmpeg  -i' +video+' -ss'+ start +'-t'+ finish, shell=True)
 
 def receive_video(save_path, server_port):
     sock.bind((server_address, server_port))
